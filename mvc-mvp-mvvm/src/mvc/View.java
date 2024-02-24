d7e13c472fdeb1e9ac82a63f51510f1194fad2ed
alfayohannis <alfa.yohannis@york.ac.uk>
2024-02-24 15:52:53 +0000

    Add MVC and MVP

--- mvc-mvp-mvvm/src/mvc/View.java
+++ mvc-mvp-mvvm/src/mvc/View.java
@@ -1,83 +1,95 @@
-package mvvm;
+package mvc;
 
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class View extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JPanel contentPane;
-	private List<Binder> bindList = new ArrayList<>();
+
+	public JPanel getContentPane() {
+		return contentPane;
+	}
+
 	private JSpinner spinner001;
 	private JSpinner spinner003;
 	private JSpinner spinner002;
-
-//	/**
-//	 * Launch the application.
-//	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					View frame = new View();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
+	private String modelName;
 
 	/**
 	 * Create the frame.
 	 */
 	public View() {
-		setTitle("MVVM: Model-View-ViewModel");
+		setTitle("MVP: Model-View-Controller");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		
+
 		spinner001 = new JSpinner();
 		spinner001.setName("spinner001");
 		((JSpinner.DefaultEditor) spinner001.getEditor()).getTextField().setColumns(3);
 		spinner001.setFont(new Font("Dialog", Font.BOLD, 32));
 		contentPane.add(spinner001);
-		
+
 		spinner002 = new JSpinner();
 		spinner002.setName("spinner002");
 		((JSpinner.DefaultEditor) spinner002.getEditor()).getTextField().setColumns(3);
 		spinner002.setFont(new Font("Dialog", Font.BOLD, 32));
 		contentPane.add(spinner002);
-		
+
 		spinner003 = new JSpinner();
 		spinner003.setName("spinner003");
 		((JSpinner.DefaultEditor) spinner003.getEditor()).getTextField().setColumns(3);
 		spinner003.setFont(new Font("Dialog", Font.BOLD, 32));
 		contentPane.add(spinner003);
-		
+
 		Point centerPoint = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
 		this.setLocation(centerPoint.x - (int) this.getSize().getWidth() / 2,
 				centerPoint.y - (int) this.getSize().getHeight() / 2);
-	}
-	
-	public void bind(ViewModel viewModel) {
-		
-			bindList.add(new Binder(spinner001, viewModel.getViewModelProperties().get(0)));
-			bindList.add(new Binder(spinner002, viewModel.getViewModelProperties().get(1)));
-			bindList.add(new Binder(spinner003, viewModel.getViewModelProperties().get(2)));
-		
+
+		spinner001.addChangeListener(new JSpinnerChangeListenger());
+		spinner002.addChangeListener(new JSpinnerChangeListenger());
+		spinner003.addChangeListener(new JSpinnerChangeListenger());
+
 	}
 
+	class JSpinnerChangeListenger implements ChangeListener {
+		boolean isListening = true;
+
+		@Override
+		public void stateChanged(ChangeEvent event) {
+			if (isListening == false) {
+				isListening = true;
+				return;
+			}
+			JSpinner spinner = (JSpinner) event.getSource();
+			int value = (int) spinner.getValue();
+			isListening = false;
+			Controller.handleChange(View.this.getName(), spinner.getName(), value);
+			
+
+		}
+
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
 }
--- mvc-mvp-mvvm/src/mvp/View.java
+++ mvc-mvp-mvvm/src/mvp/View.java
@@ -1,47 +1,29 @@
-package mvvm;
+package mvp;
 
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeListener;
 
 public class View extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	
 	private JPanel contentPane;
-	private List<Binder> bindList = new ArrayList<>();
 	private JSpinner spinner001;
 	private JSpinner spinner003;
 	private JSpinner spinner002;
-
-//	/**
-//	 * Launch the application.
-//	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					View frame = new View();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
 
 	/**
 	 * Create the frame.
 	 */
 	public View() {
-		setTitle("MVVM: Model-View-ViewModel");
+		setTitle("MVP: Model-View-Presenter");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -66,18 +48,18 @@ public class View extends JFrame {
 		((JSpinner.DefaultEditor) spinner003.getEditor()).getTextField().setColumns(3);
 		spinner003.setFont(new Font("Dialog", Font.BOLD, 32));
 		contentPane.add(spinner003);
-		
+
 		Point centerPoint = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
 		this.setLocation(centerPoint.x - (int) this.getSize().getWidth() / 2,
 				centerPoint.y - (int) this.getSize().getHeight() / 2);
+		
+		
 	}
-	
-	public void bind(ViewModel viewModel) {
-		
-			bindList.add(new Binder(spinner001, viewModel.getViewModelProperties().get(0)));
-			bindList.add(new Binder(spinner002, viewModel.getViewModelProperties().get(1)));
-			bindList.add(new Binder(spinner003, viewModel.getViewModelProperties().get(2)));
-		
+
+	public void setChangeListener(ChangeListener presenter) {
+		spinner001.addChangeListener(presenter);
+		spinner002.addChangeListener(presenter);
+		spinner003.addChangeListener(presenter);
 	}
 
 }
